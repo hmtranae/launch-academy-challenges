ef03ab731d5389c6637279b55d28f8b565b956a6
Hieu Tran <hmtran.ae@gmail.com>
2019-04-12 16:01:19 -0400

    test with node and yarn

---
+++ try-node/app.js
@@ -0,0 +1,5 @@
+// app.js
+const chewbaccaGrowl = require('./chewbaccaGrowl');
+
+chewbaccaGrowl();
+chewbaccaGrowl();
---
+++ try-node/chewbaccaGrowl.js
@@ -0,0 +1,9 @@
+// chewbaccaGrowl.js
+
+let growlText = 'Roooarrgh ur r';
+const chewbaccaGrowl = () => {
+  growlText = `${growlText}oo`;
+  console.log(growlText);
+};
+
+module.exports = chewbaccaGrowl;
