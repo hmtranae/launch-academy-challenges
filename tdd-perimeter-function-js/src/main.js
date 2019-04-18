2056d6fc3f5ec968776b55b2359cac0e422dc252
Hieu Tran <hmtran.ae@gmail.com>
2019-04-18 09:08:33 -0400

    refactor if else statement in perimeter function

--- tdd-perimeter-function-js/src/main.js
+++ tdd-perimeter-function-js/src/main.js
@@ -1,14 +1,11 @@
 // your code, here
-let perimeter = (length, width) => {
-    if (width === 0 && length === 0) {
-        return `Both values can't be 0!`
-    } else if (length === 0 || width === 0) {
-        return 1
-    } else {
-        return 2 * (length + width);
-    }
-}
+const perimeter = (length, width) => {
+  if (width === 0 && length === 0) {
+    return 'Both values can\'t be 0!';
+  }
+  return 2 * (length + width);
+};
 
 perimeter(0, 1);
 
-module.exports = perimeter
+module.exports = perimeter;
